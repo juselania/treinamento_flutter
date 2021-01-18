5efc03ef808f4f6915d8e28d624fc2aa83990036
Juselania Silva <juselania98@outlook.com>
2021-01-17 21:00:27 -0300

    parte 2 em andamento

--- treinamento_flutter/lib/home.dart
+++ treinamento_flutter/lib/home.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:treinamento_flutter/contact.dart';
-
 import 'package:treinamento_flutter/contatoservice.dart';
-import 'package:treinamento_flutter/main.dart';
-
-
 import 'models/contato.dart';
 
 class HomeApp extends StatefulWidget {
@@ -30,6 +26,7 @@ class _HomeAppState extends State<HomeApp> {
         child: ListView.builder(
           itemCount: user.length,
           itemBuilder: (context, index) {
+            Divider(color: Colors.black,);
             return InkWell(
               onTap: () {
                 Navigator.push(
@@ -44,6 +41,7 @@ class _HomeAppState extends State<HomeApp> {
                 ),
                 title: Text(user[index].nome),
                 subtitle: Text(user[index].telefone),
+              
               ),
             );
           },
--- treinamento_flutter/lib/models/contato.dart
+++ treinamento_flutter/lib/models/contato.dart
@@ -10,12 +10,21 @@ class Contato {
   String email;
   String telefone;
 
+  String get inicial {
+    var iniciais = nome.split(' ');
+    for (var i = 0; i < iniciais.length; i++) {
+      var palavra = iniciais[i];
+      print(palavra.substring(0, 1));
+      return nome[0]+sobreNome[0];
+    }
+  }
+  
+
   Contato(
       {this.nome,
       this.nascimento,
       this.idade,
       this.peso,
       this.email,
-      this.telefone
-  });
+      this.telefone});
 }
